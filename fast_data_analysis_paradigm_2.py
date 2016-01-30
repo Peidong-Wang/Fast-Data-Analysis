dc279daf5c727fffaf4aba96e092f146721d28d4
pozhijisi <WPD@wangpeidongs-MacBook-Pro.local>
2016-01-29 19:32:25 -0500

    Paradigm 2 update
    1. dealt with the missing value “Not Provided” / “Nan” handling
    2. still have problems when doing random forest fitting

--- fast_data_analysis_paradigm_2.py
+++ fast_data_analysis_paradigm_2.py
@@ -16,26 +16,30 @@ fullData = pd.concat([train, test], axis = 0)
 
 #step 3 (there is no step 2), View the column names / summary of the dataset
 # print fullData.columns
-# print fullData.head(10)
+# print fullData.tail(10)
 # print fullData.describe()
 
 #step 4, Identify the a) ID variables b)  Target variables c) Categorical Variables d) Numerical Variables e) Other Variables
 ID_col = ["Id"]
 target_col = ["TotalPayBenefits"]
-cat_cols = ["EmployeeName", "JobTitle", "Notes", "Agency", "Status", "Benefits", "BasePay"]
+cat_cols = ["EmployeeName", "JobTitle", "Notes", "Agency", "Status"]
 other_col = ["Type"]
 num_cols = list(set(list(fullData.columns)) - set(cat_cols) - set(ID_col) - set(target_col) - set(other_col))
 
 #step 5, Identify the variables with missing values and create a flag for those
 num_cat_cols = num_cols + cat_cols
+fullData[num_cat_cols] = fullData[num_cat_cols].replace("Not Provided", np.nan)
 for var in num_cat_cols:
     if fullData[var].isnull().any() == True:
-        print var
         fullData[var + "_NA"] = fullData[var].isnull() * 1
 
 #step 6, Impute Missing values
-fullData[num_cols] = fullData[num_cols].fillna(value = fullData[num_cols].mean(), inplace = True)
+fullData[num_cols] = fullData[num_cols].fillna(value = 0) #fullData[num_cols].mean())
 fullData[cat_cols] = fullData[cat_cols].fillna(value = -9999)
+
+for var in train.columns:
+    if fullData[var].isnull().any() == True:
+        print var
 
 #step 7, Create a label encoders for categorical variables and split the data set to train & test, further split the train data set to Train and Validate
 for var in cat_cols:
@@ -48,13 +52,9 @@ test = fullData[fullData["Type"] == "Test"]
 train["is_train"] = np.random.uniform(0, 1, len(train)) <= .75
 Train, Validate = train[train["is_train"] == True], train[train["is_train"] == False]
 
-print fullData.columns
-print fullData.head(10)
-print fullData.describe()
-
-# for var in train.columns:
-#     if fullData[var].isnull().any() == True:
-#         print var
+# print fullData.columns
+# print fullData.head(10)
+# print fullData.describe()
 
 #step 8, Pass the imputed and dummy (missing values flags) variables into the modelling process. I am using random forest to predict the class
 features = list(set(list(fullData.columns)) - set(ID_col) - set(target_col) - set(other_col))
